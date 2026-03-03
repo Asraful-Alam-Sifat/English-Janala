b9a3d83701f07b60627a8141ca643f8f95b39c9f
Asraful Alam <asrafulspersonal@gmail.com>
2026-03-03 22:12:53 +0600

    add all lessons

--- script/script.js
+++ script/script.js
@@ -9,11 +9,11 @@ const displayLesson = (lessons) =>{
     levelContainer.innerHTML = "";
     
     for(let lesson of lessons){
-        console.log(lesson);
+        // console.log(lesson);
 
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-       <button title="${lesson.lessonName}" class="btn btn-outline btn-primary"
+       <button onclick="loadLevelWord(${lesson.level_no})" title="${lesson.lessonName}" class="btn btn-outline btn-primary"
                   ><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
                   </button>    
         `;
@@ -21,6 +21,47 @@ const displayLesson = (lessons) =>{
         levelContainer.appendChild(btnDiv);
     }
 };
+
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+
+    // console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayLevelWord(data.data));
+
+}
+
+const displayLevelWord = (words) => {
+    // console.log(words)
+    const wordsContainer = document.getElementById("word-container");
+    wordsContainer.innerHTML = "";
+
+    words.forEach(word => {
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+           <h2 class="font-bold text-2xl"> ${word.word}</h2>
+           <p class="font-semibold">Meaning /Pronounciation</p>
+
+         <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+
+         <div class="flex justify-between items-center">
+           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+             <i class="fa-solid fa-circle-info"></i>
+           </button>
+           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+              <i class="fa-solid fa-volume-high"></i>
+           </button>
+         </div>
+        </div>      
+
+        `;
+
+        wordsContainer.appendChild(card);
+    });
+}
 
 loadLessons();
 
